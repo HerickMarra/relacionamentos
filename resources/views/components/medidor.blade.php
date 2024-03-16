333423ef1b39cfeb822566c447577332c97e0d03
HerickMarra <61362669+HerickMarra@users.noreply.github.com>
2024-03-16 09:59:08 -0300

    *

--- resources/views/components/medidor.blade.php
+++ resources/views/components/medidor.blade.php
@@ -1,4 +1,4 @@
 
 <div  style="height: 270px; width: 100%; display: flex; align-items: center; justify-content: center;">
-    <img src="/img/medidor/70.png" alt="">
+    <img src="/img/medidor/90.png" alt="">
 </div>
