7ee4d94012b2845fd92b6d6997cb47eddc6c744d
HerickMarra <61362669+HerickMarra@users.noreply.github.com>
2024-03-19 07:11:39 -0300

    *

--- app/Http/Controllers/app/NotificationController.php
+++ app/Http/Controllers/app/NotificationController.php
@@ -18,7 +18,7 @@ class NotificationController extends Controller
 
     public function getItens(Request $request){
         $pag = $request->pag;
-        $not = Notification::where('user_id', auth()->user()->id)->skip(0)->take(20)->orderBy('id', 'desc')->get();
+        $not = Notification::where('user_id', auth()->user()->id)->skip(0)->take(50)->orderBy('id', 'desc')->get();
         if($pag == 1){
             Notification::where('user_id', auth()->user()->id)->update([
                 'status' => 'visualizada'
