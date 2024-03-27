2b21face82ff15e0df37ad43c0cf62150e091ce9
Herick <herick.costa@consultmidia.com.br>
2024-03-27 14:19:42 -0300

    Reestruturação do App

--- resources/views/components/medidor.blade.php
+++ resources/views/components/medidor.blade.php
@@ -3,7 +3,7 @@
 <div  class="x-medidor-base">
     <div class="x-medidor-cicle">
             <div  class="x-medidor-medidores">
-                <p class="x-medidor-medidores-title">Reacionamento</p>
+                <p class="x-medidor-medidores-title">Relacionamento</p>
                 <p class="x-medidor-medidores-var" style="margin-top: 5px">90%</p>
                 <button class="x-medidor-medidores-act clearBtn" >Adicionar Emoção</button>
             </div>
