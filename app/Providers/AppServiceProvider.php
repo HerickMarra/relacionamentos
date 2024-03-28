6b7956c052a7285e1a96629c9e3a5ca787832a74
HerickMarra <61362669+HerickMarra@users.noreply.github.com>
2024-03-28 18:59:30 -0300

    *

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->share('version', '5');
+        view()->share('version', '6');
     }
 }
