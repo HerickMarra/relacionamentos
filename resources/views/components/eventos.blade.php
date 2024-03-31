aaf9834da5079ea7975a05c3044eee1760940c9a
HerickMarra <61362669+HerickMarra@users.noreply.github.com>
2024-03-31 01:09:30 -0300

    Atualização de erro ao calcular o ultimo login

--- resources/views/components/eventos.blade.php
+++ resources/views/components/eventos.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset("components/eventos/eventos.css?v=$version") }}">
 
 @if (count($events))
-    
+
 <div class="x-eventos">
     <p class="x-eventos-title">Proximos Eventos</p>
     @foreach ($events as $event)
@@ -11,11 +11,21 @@
         <div class="x-evento-desc">
             <p class="x-evento-desc-T">{{$event->title}}</p>
             <p class="x-evento-desc-D">{{date('d/m', strtotime($event->date))}}</p>
+            @if (in_array($event->act, ['link']))
+                <div class="x-evento-link">
+                    <a href="{{$event->act_link}}">
+                        <button class="clearBtn btnAct">Ver Mais</button>
+                    </a>
+                </div>
+            @endif
+
         </div>
+
+
     </div>
     @endforeach
-    
+
     <a href="" class="baseText x-eventos-vermais">Ver todos os eventos</a>
-    
+
 </div>
 @endif
