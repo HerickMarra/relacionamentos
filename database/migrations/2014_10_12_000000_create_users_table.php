46cd4b5febe812f0e721f9455603ed7179cac8b5
HerickMarra <herick.marra1@gmail.com>
2025-11-22 07:06:43 -0300

    Ajuste para Anne

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,12 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unique('username');
+
         });
+
+        
     }
 
     /**
