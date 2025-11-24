2f37e51ac2553e2f7f8fb04af4feaa134c2573c1
HerickMarra <herick.marra1@gmail.com>
2025-11-24 17:06:06 -0300

    Adicionar load no app ao trocar de página

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->share('version', '15');
+        view()->share('version', '16');
     }
 }
