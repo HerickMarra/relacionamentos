32e86b9cb4967d52c5bf1298e0c289f2639c132d
HerickMarra <herick.marra1@gmail.com>
2025-12-04 15:03:34 -0300

    *

--- resources/views/components/event/card-event.blade.php
+++ resources/views/components/event/card-event.blade.php
@@ -7,6 +7,7 @@
         <p class="x-cardevent-desc-T">{{$event->title}}</p>
         <p style="font-size:10px;" >{{$event->desc}}</p>
         <p class="x-cardventodesc-D">{{date('d/m', strtotime($event->date))}}</p>
+        <br>
         @if (in_array($event->act, ['link']))
             <div class="x-cardevent-link">
                 <a href="{{$event->act_link}}">
