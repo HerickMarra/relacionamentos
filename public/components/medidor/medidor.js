606ef31ddfe57f34ffe4ea27dc6e971a6791dda6
HerickMarra <herick.marra1@gmail.com>
2025-12-04 15:19:17 -0300

    *

--- public/components/medidor/medidor.js
+++ public/components/medidor/medidor.js
@@ -1,14 +1,14 @@
-let x_medidor_qntItens = 0, x_medidor_atual = 0, x_medidor_element = null;
+// let x_medidor_qntItens = 0, x_medidor_atual = 0, x_medidor_element = null;
 
-window.onload = function() {
-    x_medidor_element = $('.x-medidor-medidores');
-    x_Medidor_qntItens = x_medidor_element.length;
-    setInterval(()=>{
-        x_medidor_atual++;
-        if(x_medidor_atual>= 2){
-            x_medidor_atual = 0;
-        }
-        x_medidor_element.fadeOut();
-        x_medidor_element.eq(x_medidor_atual).fadeIn();
-    },30000);
-};
+// window.onload = function() {
+//     x_medidor_element = $('.x-medidor-medidores');
+//     x_Medidor_qntItens = x_medidor_element.length;
+//     setInterval(()=>{
+//         x_medidor_atual++;
+//         if(x_medidor_atual>= 2){
+//             x_medidor_atual = 0;
+//         }
+//         x_medidor_element.fadeOut();
+//         x_medidor_element.eq(x_medidor_atual).fadeIn();
+//     },30000);
+// };
